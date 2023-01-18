456b96cd8420c6ea0945b744199b2c6901b372da
Liza McLain <lizamclain@gmail.com>
2023-01-18 09:20:54 -0600

    1st deliverable see plants

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plantList, setPlantList] = useState([])
+  
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(response => response.json())
+    .then(data => setPlantList(data))
+  })
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList 
+        plantList={plantList}
+      />
     </main>
   );
 }
