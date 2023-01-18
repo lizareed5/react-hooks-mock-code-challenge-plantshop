009fb5e8097227966f1f91f9bd73cb84c738d94a
Liza McLain <lizamclain@gmail.com>
2023-01-18 09:52:48 -0600

    3rd deliverable out/in stock button

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 function PlantCard({plant}) {
+  const [isPlantInStock, setIsPlantInStock] = useState(true)
+  
+  const handleSoldOutClick = () => {
+    setIsPlantInStock(!isPlantInStock)
+  }
+
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {isPlantInStock ? (
+        <button 
+          className="primary" 
+          onClick={handleSoldOutClick}
+        >In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button
+          onClick={handleSoldOutClick}
+        >Out of Stock</button>
       )}
     </li>
   );
